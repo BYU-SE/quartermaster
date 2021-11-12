f80997faf1446187a8fd655722bb32ed980ec226
BJ Foutz <bjfoutz00@gmail.com>
2021-11-12 08:56:26 -0700

    Improve event ID management
    Still need to create test cases

--- src/event.ts
+++ src/event.ts
@@ -4,25 +4,26 @@ import { Response, TimeStats, ResponseStats } from ".";
  * An event goes through a stage. In web systems, it could be a web request.
  * Two events with the same key are identical, but could have originated from
  * two separate sources.
+ * Each event has a unique id.
  * 
  * An event keeps track of time spent in various stages.
  */
 export class Event {
-  static idCounter: number = 0;
+  private static idCounter: number = 0;
 
   public stageTimes: TimeStats[] = [];
   public response: Response | null = null;
   public responseTime: ResponseStats = new ResponseStats();
-
-  readonly id: number;
+  public readonly id: number;
   
-  constructor(public key: string) { 
-    this.id = Event.idCounter;
-    Event.idCounter++;
-  }
+  constructor(public key: string) { this.id = ++Event.idCounter; }
 
   addStageTime(t: TimeStats): void {
     this.stageTimes.push(t);
   }
-  
+
+  public static resetIDCounter(): void {
+    this.idCounter = 0;
+  }
+
 }
